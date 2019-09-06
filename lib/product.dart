da3b3bb2645cbb1d3208225e42315c6ad60b5207
Josh <joshuajames355@gmail.com>
2019-09-06 23:25:27 +0100

    Added full screen product description.

--- lib/product.dart
+++ lib/product.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import "dart:math";
 
 final double imageSize = 120;
+final double largeImageSize = 250;
 
 class Product
 {
@@ -12,13 +14,16 @@ class Product
   final String category;
 
   final Widget icon;
+  final Widget iconLarge;
 
   //Loads Image from file
   Product({this.title, this.description, this.price, this.category, this.iconName}) : icon = Image(
       image: AssetImage("images/" + iconName),
       width: imageSize,
-      height: imageSize,
-      );
+      height: imageSize) ,iconLarge = Image(image: AssetImage("images/" + iconName),
+
+      width: largeImageSize,
+      height: largeImageSize);
 
 
   Product.fromNet({this.title, this.description, this.price, this.category, this.iconName, String apiBaseUrl}) : icon = CachedNetworkImage(
@@ -26,7 +31,12 @@ class Product
       placeholder: (context, url) => CircularProgressIndicator(),
       width: imageSize,
       height: imageSize,
-    );  
+    ), iconLarge = CachedNetworkImage(
+      imageUrl: apiBaseUrl + "/images/" + iconName, //Todo: Actually link to backend
+      placeholder: (context, url) => CircularProgressIndicator(),
+      width: largeImageSize,
+      height: largeImageSize,  
+    );
 }
 
 //Displayed in the main list views
@@ -41,43 +51,162 @@ class ProductWidget extends StatelessWidget
     return Align(
     alignment: Alignment.topLeft,
     child: 
-      Container
+      FlatButton
       (
-        decoration: BoxDecoration
+        onPressed: ()
+        {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => ProductWidgetFullScreen(product: product,)));
+        },
+        child: Container
         (
-          border: Border.all(color: Colors.black),
-        ),
-        width: imageSize + 50,
-        height: imageSize + 50,
-        alignment: Alignment(0,0),
-        margin: EdgeInsets.all(3),
-        child: Column
-        (
-          children: [
-            Center
-            (
-              child: Text
+          decoration: BoxDecoration
+          (
+            border: Border.all(color: Colors.black),
+          ),
+          width: imageSize + 50,
+          height: imageSize + 50,
+          alignment: Alignment(0,0),
+          margin: EdgeInsets.all(3),
+          child: Column
+          (
+            children: [
+              Center
               (
-                product.title,
-                style: TextStyle
+                child: Text
                 (
-                  fontSize: 18
+                  product.title,
+                  overflow: TextOverflow.ellipsis,
                 ),
-                overflow: TextOverflow.ellipsis,
               ),
-            ),
-            product.icon,
-            Container
-            (
-              margin: EdgeInsets.symmetric(horizontal: 5),
-              child: Row
+              product.icon,
+              Container
               (
-                children: [Text(product.price)]
+                margin: EdgeInsets.symmetric(horizontal: 5),
+                child: Row
+                (
+                  children: [Text(product.price)]
+                )
               )
-            )
-          ]
-        ),
+            ]
+          ),
+        )
       )
     );
+  }
+}
+
+class ProductWidgetFullScreen extends StatefulWidget
+{
+  const ProductWidgetFullScreen({ Key key, this.product}) : super(key: key);
+
+  final Product product;
+
+  @override
+  _ProductWidgetFullScreenState createState() => _ProductWidgetFullScreenState();
+}
+
+class _ProductWidgetFullScreenState extends State<ProductWidgetFullScreen>{
+  int count = 1;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar
+      (
+        title: Text(widget.product.title),
+        leading: FlatButton
+        (
+          child: Icon(IconData(58820, fontFamily: 'MaterialIcons', matchTextDirection: true)),
+          onPressed: ()
+          {
+            Navigator.pop(context);
+          },
+          ),
+      ),
+      body: Align(
+        alignment: Alignment.topLeft,
+        child: 
+          Container
+          (
+            alignment: Alignment(0,0),
+            margin: EdgeInsets.all(3),
+            child: Column
+            (
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                widget.product.iconLarge,
+                Container
+                (
+                  margin: EdgeInsets.symmetric(horizontal: 50),
+                  child: Text(widget.product.description)
+                ),
+                Container
+                (
+                  margin: EdgeInsets.symmetric(horizontal: 50),
+                  child: Row
+                  (
+                    children: [Text(widget.product.price)]
+                  )
+                ),
+                Container
+                (
+                  margin: EdgeInsets.symmetric(horizontal: 5),
+                  child: Row
+                  (
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      FloatingActionButton(
+                        heroTag: "btn1",
+                        child: new Icon(IconData(0xe15b, fontFamily: 'MaterialIcons'), color: Colors.black,),
+                        backgroundColor: Colors.white,
+                        onPressed: () 
+                        {
+                          setState(() 
+                          {
+                            count = max(1, count - 1);
+                          });
+                        }
+                      ),
+                      Text(
+                        count.toString(),
+                        style: TextStyle
+                        (
+                          fontSize: 24
+                        ),
+                      ),
+                      FloatingActionButton(
+                        heroTag: "btn2",
+                        child: new Icon(Icons.add, color: Colors.black,),
+                        backgroundColor: Colors.white,
+                        onPressed: () 
+                        {
+                          setState(() 
+                          {
+                            count = count + 1;
+                          });
+                        }
+                      ),]
+                  )
+                ),
+                RaisedButton
+                (
+                  onPressed: ()
+                  {
+                    //Todo basket
+                    Navigator.pop(context);
+                  },
+                  child: Padding
+                  (
+                    padding: EdgeInsets.all(15),
+                    child: Text("Add To Basket",
+                      style: TextStyle(fontSize: 24),
+                    )
+                  ),
+                )
+              ]
+            ),
+          )
+        )
+    );    
   }
 }

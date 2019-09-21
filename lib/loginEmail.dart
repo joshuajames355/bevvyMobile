4ba770df4c07d0e3695728d45c796c833505dda8
Josh <joshuajames355@gmail.com>
2019-09-21 01:33:33 +0100

    Minor adjustments to Login with email dialog.
    Refactored sign in/ create account logic out of UI code, and made minor aesthetic + usability adjustments.

--- lib/loginEmail.dart
+++ lib/loginEmail.dart
@@ -20,7 +20,7 @@ class _LoginEmailPageState extends State<LoginEmailPage> {
   final _passwordFocus = FocusNode();
   var _obscureText = true;
 
-   @override 
+  @override 
   Widget build(BuildContext context)
   {
     return SimpleDialog(
@@ -29,6 +29,7 @@ class _LoginEmailPageState extends State<LoginEmailPage> {
           borderRadius: BorderRadius.circular(12),
       ),      
       children: dialogContent(context),
+      contentPadding: EdgeInsets.all(7),
     );
   }
 
@@ -54,33 +55,37 @@ class _LoginEmailPageState extends State<LoginEmailPage> {
           FocusScope.of(context).requestFocus(_passwordFocus);
         },
       ),
-      TextField
+      Container
       (
-        controller: _passwordTextController,
-        focusNode: _passwordFocus,
-        textInputAction: TextInputAction.send,
-        keyboardType: TextInputType.text,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(
-            borderSide: BorderSide(color: Colors.black, width: 5)
-          ),
-          labelText: 'Password',
-          suffixIcon: FlatButton(
-            child: Icon(visibilityIcon),
-            onPressed: ()
-            {
-              setState(() 
+        margin: EdgeInsets.only(bottom: 10),
+        child: TextField
+        (
+          controller: _passwordTextController,
+          focusNode: _passwordFocus,
+          textInputAction: TextInputAction.go,
+          keyboardType: TextInputType.text,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black, width: 5)
+            ),
+            labelText: 'Password',
+            suffixIcon: FlatButton(
+              child: Icon(visibilityIcon),
+              onPressed: ()
               {
-                _obscureText = !_obscureText;
-              });
-            },
-          )
+                setState(() 
+                {
+                  _obscureText = !_obscureText;
+                });
+              },
+            )
+          ),
+          obscureText: _obscureText,
+          onSubmitted: (String password)
+          {
+            signIn();
+          },
         ),
-        obscureText: _obscureText,
-        onSubmitted: (v)
-        {
-          FocusScope.of(context).requestFocus(_passwordFocus);
-        },
       ),
       Row
       (
@@ -91,78 +96,28 @@ class _LoginEmailPageState extends State<LoginEmailPage> {
           (
             child: RaisedButton
             (
+              shape: RoundedRectangleBorder(borderRadius: new BorderRadius.all(Radius.circular(10))),
+              color: Theme.of(context).primaryColor,
               child: Padding
               (
                 child: Text("Sign In"),
                 padding: EdgeInsets.symmetric(vertical: 20),
               ),
-              onPressed: ()
-              {
-                if(_emailTextController.text == "" || _passwordTextController.text == "" || _emailTextController.text == null || _passwordTextController.text == null) return;
-
-                auth.signInWithEmailAndPassword(email: _emailTextController.text, password: _passwordTextController.text).then((AuthResult result)
-                {
-                  widget.onLogin(result.user);
-                }).catchError((e)
-                {
-                  if(e.code == "ERROR_USER_NOT_FOUND")
-                  {
-                    showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("User not found.")));
-                  }
-                  else if(e.code == "ERROR_WRONG_PASSWORD")
-                  {
-                    showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("Wrong password.")));
-                  }
-                  else if(e.code == "ERROR_INVALID_EMAIL")
-                  {
-                    showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("The Email address is invalid.")));
-                  }
-                  else if(e.code == "ERROR_TOO_MANY_REQUESTS ")
-                  {
-                    showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("Too many request.\n Try again later.")));
-                  }
-                  else if(e.code == "ERROR_USER_DISABLED")
-                  {
-                    showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("This account has been disabled.")));
-                  }
-                });           
-              },
+              onPressed: signIn,
             ),
           ),
           Expanded
           (
             child: RaisedButton
             (
+              color: Theme.of(context).primaryColor,
+              shape: RoundedRectangleBorder(borderRadius: new BorderRadius.all(Radius.circular(10))),
               child: Padding
               (
                 child: Text("Create Account"),
                 padding: EdgeInsets.symmetric(vertical: 20),
               ),
-              onPressed: ()
-              {
-                if(_emailTextController.text == "" || _passwordTextController.text == "" || _emailTextController.text == null || _passwordTextController.text == null) return;
-
-                auth.createUserWithEmailAndPassword(email: _emailTextController.text, password: _passwordTextController.text).then((AuthResult result)
-                {
-                  result.user.sendEmailVerification();
-                  widget.onLogin(result.user);
-                }).catchError((e)
-                {
-                  print(e.code);
-                  if(e.code == "ERROR_EMAIL_ALREADY_IN_USE")
-                  {
-                    showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("Email Already in Use.")));
-                  }
-                  else if(e.code == "ERROR_WEAK_PASSWORD")
-                  {
-                    showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("Password too weak.\nIt must be at least 6 characters.")));
-                  }
-                  else if(e.code == "ERROR_INVALID_EMAIL")
-                  {
-                    showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("The Email address is invalid.")));
-                  }
-                });                
-              },
+              onPressed: createAccount
             ),
           ),
         ],
@@ -170,13 +125,78 @@ class _LoginEmailPageState extends State<LoginEmailPage> {
     ];
   }
 
-    @override
-  void dispose() {
+  signIn()
+  {
+    if(_emailTextController.text == "" || _passwordTextController.text == "" || _emailTextController.text == null || _passwordTextController.text == null) 
+    {
+      showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("The email and password fields must not be empty.")));
+      return;
+    }
+
+    auth.signInWithEmailAndPassword(email: _emailTextController.text, password: _passwordTextController.text).then((AuthResult result)
+    {
+      widget.onLogin(result.user);
+    }).catchError((e)
+    {
+      if(e.code == "ERROR_USER_NOT_FOUND")
+      {
+        showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("User not found.")));
+      }
+      else if(e.code == "ERROR_WRONG_PASSWORD")
+      {
+        showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("Wrong password.")));
+      }
+      else if(e.code == "ERROR_INVALID_EMAIL")
+      {
+        showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("The Email address is invalid.")));
+      }
+      else if(e.code == "ERROR_TOO_MANY_REQUESTS ")
+      {
+        showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("Too many request.\n Try again later.")));
+      }
+      else if(e.code == "ERROR_USER_DISABLED")
+      {
+        showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("This account has been disabled.")));
+      }
+    });           
+  }
+
+  createAccount()
+  {
+    if(_emailTextController.text == "" || _passwordTextController.text == "" || _emailTextController.text == null || _passwordTextController.text == null) 
+    {
+      showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("The email and password fields must not be empty.")));
+      return;
+    }
+
+    auth.createUserWithEmailAndPassword(email: _emailTextController.text, password: _passwordTextController.text).then((AuthResult result)
+    {
+      result.user.sendEmailVerification();
+      widget.onLogin(result.user);
+    }).catchError((e)
+    {
+      print(e.code);
+      if(e.code == "ERROR_EMAIL_ALREADY_IN_USE")
+      {
+        showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("Email Already in Use.")));
+      }
+      else if(e.code == "ERROR_WEAK_PASSWORD")
+      {
+        showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("Password too weak.\nIt must be at least 6 characters.")));
+      }
+      else if(e.code == "ERROR_INVALID_EMAIL")
+      {
+        showDialog(context: context, builder: (context) => AlertDialog(title: Text("Error"), content: Text("The Email address is invalid.")));
+      }
+    }); 
+  }
+
+  @override
+  void dispose() 
+  {
     // Clean up the controller when the widget is disposed.
     _emailTextController.dispose();
     _passwordTextController.dispose();
     super.dispose();
   }
-
 }
-

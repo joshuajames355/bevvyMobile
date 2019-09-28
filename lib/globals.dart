740e17dc1039a35d153ba8d921a09499615057a7
Josh <joshuajames355@gmail.com>
2019-09-28 02:49:15 +0100

    Removed google sign in.

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,7 +1,5 @@
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 final FirebaseAnalytics analytics = FirebaseAnalytics();
 final FirebaseAuth auth = FirebaseAuth.instance;
-final GoogleSignIn googleSignIn = GoogleSignIn();
--- lib/reauthenticate.dart
+++ lib/reauthenticate.dart
@@ -1,32 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:bevvymobile/reauthEmail.dart';
-import 'package:bevvymobile/globals.dart';
-import 'package:google_sign_in/google_sign_in.dart';
 
 Future<void> reauthenticate(BuildContext context ,FirebaseUser user) async
 {
-  bool hasGoogle = false;
   bool hasEmail = false;
   user.providerData.forEach((UserInfo userInfo)
   {
-    if(userInfo.providerId == "google.com") hasGoogle = true;
     if(userInfo.providerId == "password") hasEmail = true;
   });
 
   AuthCredential credentials;
 
-  if(hasGoogle)
-  {
-    final GoogleSignInAccount googleUser = await googleSignIn.signIn();
-    final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
-    credentials = GoogleAuthProvider.getCredential
-    (
-      accessToken: googleAuth.accessToken,
-      idToken: googleAuth.idToken,
-    );
-  }
-  else if(hasEmail)
+  if(hasEmail)
   {
     credentials = await showDialog(context: context, builder: (BuildContext context) => ReauthEmailPage());
   }

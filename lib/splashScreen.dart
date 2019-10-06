467c552ba1b227a054cd6200b74f0cee79bf5910
Josh <joshuajames355@gmail.com>
2019-10-06 12:59:06 +0100

    First attempt at animated splash screen.

--- lib/splashScreen.dart
+++ lib/splashScreen.dart
@@ -10,16 +10,71 @@ class SplashScreen extends StatefulWidget
   _SplashScreenState createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen>
+class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin
 {
 
   TextEditingController _controller = TextEditingController();
+  List<Widget> images = [
+    SplashScreenImage(desription: "Description 1. Our company is called Jovi, maybe we should do something.", imageFilename: "images/smirnoff.jpg",),
+    SplashScreenImage(desription: "Description 2", imageFilename: "images/jd.jpg",)
+    ];
+  AnimationController animationController;
+  Animation<double> opacityAnimation;
+  Animation<double> imageAnimation;
+  int currentImage = 0;
+
+  @override
+  void initState() {
+    animationController = AnimationController(duration: const Duration(milliseconds: 500), vsync: this);
+    imageAnimation = Tween<double>(begin: 0, end: 300).animate(CurvedAnimation(parent: animationController, curve: Curves.easeIn, reverseCurve: Curves.easeOut));
+    opacityAnimation = Tween<double>(begin: 1, end: 0).animate(CurvedAnimation(parent: animationController, curve: Curves.easeIn, reverseCurve: Curves.easeOut));
+
+    imageAnimation.addStatusListener((AnimationStatus state)
+    {
+      if(state == AnimationStatus.dismissed)
+      {
+        Future.delayed(Duration(seconds: 5)).then((x)
+        {
+          animationController.forward();
+        });
+      }
+      if(state == AnimationStatus.completed)
+      {
+        setState(()
+        {
+          currentImage = (currentImage + 1) % images.length;
+        });
+        animationController.reverse();
+      }
+    });
+
+    imageAnimation.addListener(()
+    {
+      setState(() {});
+    });
+
+    Future.delayed(Duration(seconds: 5)).then((x)
+    {
+      animationController.forward();
+    });
+
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context)
   {
+    
     return Scaffold
     (
+      appBar: AppBar
+      (
+        backgroundColor: Theme.of(context).primaryColorLight,
+        title: Center
+        (
+          child: Text("Jovi", style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold))
+        ),
+      ),
       body: Padding
       (
         padding: EdgeInsets.all(12),
@@ -27,46 +82,60 @@ class _SplashScreenState extends State<SplashScreen>
         (
           children: <Widget>
           [
-            Expanded
+            Flexible
             (
-              child: Image
+              child: Transform.translate
               (
-                image: AssetImage(
-                    'images/icon.jpg',
-                    ),
-              )
+                child: Opacity
+                (
+                  opacity: opacityAnimation.value,
+                  child: images[currentImage]
+                ),
+                offset: Offset(imageAnimation.value,0),
+              ),
+              fit: FlexFit.loose
             ),
             TextField
             (
-              autofocus: true,
+              autofocus: false,
               controller: _controller,
               textInputAction: TextInputAction.done,
               keyboardType: TextInputType.emailAddress,
               decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black, width: 5)
-                ),
+                border: UnderlineInputBorder(),
+                 // borderSide: BorderSide(color: Colors.white, width: 5)
                 labelText: 'Email Address',
               ),
               onSubmitted: (v) => onSubmit(),
             ),
-            RaisedButton
+            Padding
             (
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),      
-              padding: EdgeInsets.all(12),
-              color: Theme.of(context).primaryColor,
-              child: Container
+              padding: EdgeInsets.only(top: 8),
+              child: RaisedButton
               (
-                width: double.infinity,
-                child: Center
+                padding: EdgeInsets.all(12),
+                color: Theme.of(context).primaryColor,
+                child: Container
                 (
-                  child: Text("Login in or signup"),
+                  width: double.infinity,
+                  child: Center
+                  (
+                    child: Text("Login in or signup"),
+                  ),
                 ),
+                onPressed: () => onSubmit(),
               ),
-              onPressed: () => onSubmit(),
-            )
+            ),
+            MediaQuery.of(context).viewInsets.bottom==0.0 ? 
+              Expanded
+              (
+                child: Container(),
+              ) :
+              Container(),
+            MediaQuery.of(context).viewInsets.bottom==0.0 ? 
+              Text("Some boring legal stuff. Will probably fill this is later with something more usefull, but until then we have this random stream of conscionness where lorem ipsum would have probably been more appropriate.",
+                  style: TextStyle(fontSize: 8),) :
+                Container(),
           ],
         )
       )
@@ -87,5 +156,51 @@ class _SplashScreenState extends State<SplashScreen>
         Navigator.pushNamed(context, "/createAccount", arguments: _controller.text);
       }
     });
+  }
+
+  @override
+  void dispose()
+  {
+    animationController.dispose();
+    _controller.dispose();
+    super.dispose();
+  }
+}
+
+class SplashScreenImage extends StatelessWidget
+{
+  const SplashScreenImage({ Key key, this.imageFilename, this.desription}) : super(key: key);
+
+  final String imageFilename;
+  final String desription;
+
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Container
+    (
+      child: Column
+      (
+        children: 
+        [ 
+          Flexible
+          (
+            child: Image
+            (
+              height: 200,
+              width: 200,
+              image: AssetImage(imageFilename),
+            ),
+            fit: FlexFit.loose,
+          ),
+          Padding
+          (
+            child: Text(desription),
+            padding: EdgeInsets.all(3),
+          )
+        ]
+      )
+    );
   }
 }

b5a894b2e0cd5e86898016dfce84b6671f29e700
Josh <joshuajames355@gmail.com>
2019-10-09 16:20:57 +0100

    Adjusted ProductWidget.

--- lib/productWidget.dart
+++ lib/productWidget.dart
@@ -13,58 +13,56 @@ class ProductWidget extends StatelessWidget
     return Hero
     (
       tag: product.id,
-      child: FlatButton
-      (
-        onPressed: ()
-        {
-          Navigator.pushNamed(context, "/product", arguments: product);
-        },
-        padding: EdgeInsets.all(5),
-        
-        child: Container
+      child: Card(
+        child: 
+        FlatButton
         (
-          decoration: BoxDecoration
+          onPressed: ()
+          {
+            Navigator.pushNamed(context, "/product", arguments: product);
+          },
+          padding: EdgeInsets.all(5),
+          
+          child: Container
           (
-            borderRadius: new BorderRadius.all(Radius.circular(12)),
-            color: Theme.of(context).primaryColorLight,
-          ),
-          width: imageSize + 50,
-          height: imageSize + 100,
-          child: Column
-          (
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              Center
-              (
-                child: Text
+            width: imageSize + 50,
+            height: imageSize + 100,
+            child: Column
+            (
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Center
                 (
-                  product.title,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  child: Text
+                  (
+                    product.title,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
                 ),
-              ),
-              Center
-              (
-                child: Text
+                Center
                 (
-                  product.size + "•" + product.priceCategory + "•" + product.category,
-                  overflow: TextOverflow.ellipsis,
-                  style: TextStyle(fontSize: 12),
+                  child: Text
+                  (
+                    product.size + "•" + product.priceCategory + "•" + product.category,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(fontSize: 12),
+                  ),
                 ),
-              ),
-              Expanded
-              (
-                child: product.icon
-              ),
-              Container
-              (
-                margin: EdgeInsets.all(5),
-                child: Row
+                Expanded
                 (
-                  children: [Text("£" + product.price.toStringAsFixed(2), style: TextStyle(fontSize: 12),)]
+                  child: product.icon
+                ),
+                Container
+                (
+                  margin: EdgeInsets.all(5),
+                  child: Row
+                  (
+                    children: [Text("£" + product.price.toStringAsFixed(2), style: TextStyle(fontSize: 12),)]
+                  )
                 )
-              )
-            ]
+              ]
+            )
           )
         )
       )

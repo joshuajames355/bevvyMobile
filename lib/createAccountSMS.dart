cef51f80f28b3a5bc5eed3bd7efdb30d43b57b26
Alex Stuckey <alex@stuckey.org.uk>
2019-10-10 20:30:01 +0100

    Removing navigation from createAccountSMS

--- lib/createAccountSMS.dart
+++ lib/createAccountSMS.dart
@@ -235,15 +235,6 @@ class _CreateAccountSMSState extends State<CreateAccountSMS>
       // "Helper object that contains the result of a successful sign-in, link and reauthenticate action."
 
       // result.user is an FIRUser object
-
-      if(false) //TODO: If account has completed onboarding
-      {
-        Navigator.pushNamed(context, "/home");
-      }
-      else
-      {
-        Navigator.pushNamed(context, "/createAccount", arguments: result.user);
-      }
     }).catchError((e)
     {
       if(e.code == "ERROR_INVALID_CREDENTIAL")

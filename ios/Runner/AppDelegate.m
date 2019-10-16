e14122f9b24e25b611bd843936aef6e80ac15c1c
Alex Stuckey <alex@stuckey.org.uk>
2019-10-16 23:13:00 +0100

    Google Maps iOS install

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,10 +1,12 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import "GoogleMaps/GoogleMaps.h"
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  [GMSServices provideAPIKey:@"AIzaSyBbaW0SyM9LcEiDPGNeMxmtQB0vK8Hhh-o"];
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];

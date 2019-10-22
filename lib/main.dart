1b45e03c6fab08abff6bfe6efada734f92fddc0e
Alex Stuckey <alex@stuckey.org.uk>
2019-10-22 11:31:03 +0100

    Removing main() from common main file, and its contents to the requisite main_{ENVS}

--- lib/main.dart
+++ lib/main.dart
@@ -416,16 +416,3 @@ class _AppState extends State<App>{
     });  
   }
 }
-
-void main() {
-  // Set `enableInDevMode` to true to see reports while in debug mode
-  // This is only to be used for confirming that reports are being
-  // submitted as expected. It is not intended to be used for everyday
-  // development.
-  Crashlytics.instance.enableInDevMode = true;
-
-  // Pass all uncaught errors from the framework to Crashlytics.
-  FlutterError.onError = Crashlytics.instance.recordFlutterError;
-
-  runApp(App());
-}
--- lib/main_dev.dart
+++ lib/main_dev.dart
@@ -2,12 +2,23 @@
 import "config.dart";
 import "main.dart";
 import 'package:flutter/material.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+
 
 void main() {
   var configuredApp = new AppConfig(
     apiBaseUrl: 'http://192.168.1.16/app',
     child: new App(),
   );
+
+  // Set `enableInDevMode` to true to see reports while in debug mode
+  // This is only to be used for confirming that reports are being
+  // submitted as expected. It is not intended to be used for everyday
+  // development.
+  Crashlytics.instance.enableInDevMode = true;
+
+  // Pass all uncaught errors from the framework to Crashlytics.
+  FlutterError.onError = Crashlytics.instance.recordFlutterError;
 
   runApp(configuredApp);
 }
